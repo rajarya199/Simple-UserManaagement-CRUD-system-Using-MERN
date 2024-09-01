41d3a31d7859dbeedd9d1c00fdc4048b61671b92
rajarya199 <aryalrajan078@gmail.com>
2024-09-01 16:23:23 +0545

    modify style table

--- client/src/components/AllUser.jsx
+++ client/src/components/AllUser.jsx
@@ -1,6 +1,25 @@
 import React ,{useEffect,useState} from 'react'
-import{Table,TableBody,TableCell,TableHead,TableRow} from '@mui/material'
+import{Table,TableBody,TableCell,TableHead,TableRow,styled,Button} from '@mui/material'
 import { getUsers } from '../service/api'
+
+const Styledtable=styled(Table)`
+width:90%;
+margin :50px auto 0 auto;
+`
+
+const Thead=styled(TableRow)`
+background:#000000;
+& >th{
+color:#fff;
+font-size:20px;
+}
+`
+const TBody=styled(TableRow)`
+  & >td{
+  font-size:20px;
+
+  }
+`
 const AllUser = () => {
   const[users,setUsers]=useState([])
 useEffect(()=>{
@@ -15,35 +34,44 @@ const getAllUsers= async()=>{
 }
   return (
     <>
-    <Table>
+    <Styledtable>
       <TableHead>
-        <TableRow>
+        <Thead>
           <TableCell>Id </TableCell>
           <TableCell>Name</TableCell>
           <TableCell>Username</TableCell>
           <TableCell>E-mail</TableCell>
           <TableCell> Phone</TableCell>
+          <TableCell>Actions</TableCell>
 
-        </TableRow>
+
+
+        </Thead>
 
       </TableHead>
       <TableBody>
         {
           users.map((user,i)=>(
-            <TableRow key={i}>
+            <TBody key={i}>
               <TableCell> {i} </TableCell>
               <TableCell> {user.name} </TableCell>
               <TableCell> {user.username} </TableCell>
               <TableCell> {user.email} </TableCell>
               <TableCell> {user.phone} </TableCell>
+              <TableCell> 
+                <Button variant='contained' style={{marginRight:10}}>Edit</Button>
+                <Button variant='contained' color='error'>Delete</Button>
 
-            </TableRow>
+              </TableCell>
+
+
+            </TBody>
             
           ))
         }
 
       </TableBody>
-    </Table>
+    </Styledtable>
     </>
   )
 }
